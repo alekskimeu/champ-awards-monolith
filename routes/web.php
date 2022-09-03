81f6403deb8ff440a06f412ee66242b3fd7fadaa
alekskimeu <kim3ual3x@gmail.com>
2022-09-03 09:20:04 +0300

    Added middleware to guard routes

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,35 @@ Route::get('/', [PollController::class, 'index']);
 Route::post('/polls/vote/{id}', [PollController::class, 'vote']);
 
 // Admin
-Route::inertia('/register', 'Register');
-Route::inertia('/login', 'Login');
-
-Route::post('/admin/register', [AdminController::class, 'register']);
-Route::post('/admin/login', [AdminController::class, 'authenticate']);
-Route::get('/admin', [AdminController::class, 'index']);
-Route::post('/admin/logout', [AdminController::class, 'logout']);
-
-// Categories 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::post('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-
-// Events 
-Route::get('/events', [EventController::class, 'index']);
-Route::post('/events', [EventController::class, 'store']);
-Route::get('/events/{id}', [EventController::class, 'show']);
-Route::post('/events/{id}', [EventController::class, 'update']);
-Route::delete('/events/{id}', [EventController::class, 'destroy']);
-
-// Participants 
-Route::get('/participants', [ParticipantController::class, 'index']);
-Route::post('/participants', [ParticipantController::class, 'store']);
-Route::post('/participants/{id}', [ParticipantController::class, 'update']);
-Route::delete('/participants/{id}', [ParticipantController::class, 'destroy']);
+Route::group(['middleware' => 'guest'], function () {
+    Route::inertia('/register', 'Register');
+    Route::inertia('/login', 'Login')->name('login');
+    Route::post('/admin/register', [AdminController::class, 'register']);
+    Route::post('/admin/login', [AdminController::class, 'authenticate']);
+});
 
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::post('/admin/logout', [AdminController::class, 'logout']);
 
+    // Categories 
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::post('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+    // Events 
+    Route::get('/events', [EventController::class, 'index']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+    Route::post('/events/{id}', [EventController::class, 'update']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+    // Participants 
+    Route::get('/participants', [ParticipantController::class, 'index']);
+    Route::post('/participants', [ParticipantController::class, 'store']);
+    Route::post('/participants/{id}', [ParticipantController::class, 'update']);
+    Route::delete('/participants/{id}', [ParticipantController::class, 'destroy']);
+});
 
